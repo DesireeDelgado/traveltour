391d59c9e8fbcbeda7ba478045a85183f1f087e8
Desiree <deshy_123@hotmail.com>
2026-05-24 14:16:03 +0200

    quitado filtros comentarios

--- src/Controller/Admin/ComentarioCrudController.php
+++ src/Controller/Admin/ComentarioCrudController.php
@@ -6,14 +6,12 @@ use App\Entity\Comentario;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class ComentarioCrudController extends AbstractCrudController
 {
@@ -41,13 +39,6 @@ class ComentarioCrudController extends AbstractCrudController
             ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
                 return $action->displayIf(static fn () => true);
             });
-    }
-
-    public function configureFilters(Filters $filters): Filters
-    {
-        return $filters
-            ->add(EntityFilter::new('id_usuario')->setLabel('Usuario'))
-            ->add(EntityFilter::new('id_viaje')->setLabel('Viaje'));
     }
 
     public function configureFields(string $pageName): iterable
